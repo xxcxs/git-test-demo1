bb3fae09f74791932b923ba295992c826c7a5a23
alfredddch@126.com <alfredddch@126.com>
2023-07-13 16:44:19 +0800

    test

--- demo01/src/main/java/Test.java
+++ demo01/src/main/java/Test.java
@@ -5,6 +5,8 @@ public class Test {
         System.out.println("asdas");
 
         System.out.println("asdas");
+
+        System.out.println("asdasd");
     }
 
 
--- demo02/src/main/java/org/example/Test02.java
+++ demo02/src/main/java/org/example/Test02.java
@@ -4,6 +4,9 @@ public class Test02 {
 
     public static void main(String[] args) {
         System.out.println("jgjhj");
+
+        System.out.println("asdadasd");
+
     }
 
 }
